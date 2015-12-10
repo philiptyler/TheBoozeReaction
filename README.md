e9c87839705e5cf4f97ce860e687f365edacd8c6
Zach Rose <zach@zachrose.biz>
2015-12-10 11:29:20 -0800

    Initial build script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # TheBoozeReaction
+
 Web Application written in React.js + backbone.js to utilize boozefinder API
+
+## Dependencies
+
+Install node.js. (We're using v0.12.5)
+`npm install` to install JavaScript dependencies
+
+## Run the tests
+
+`npm test`
+
+## Create a build
+
+`npm run build`
+
+## Serve the build
+
+`cd build && python -m SimpleHTTPServer` (for now)
+
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Running the build"
+
+# Wrangle paths
+# Set $DIR to this directory, so we can run this script from anywhere.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Add node_modules executables to our PATH
+PATH=$PATH:$DIR/node_modules/.bin
+SRC=$DIR/src
+
+# make our build directory if it doesn't exist yet
+mkdir -p build
+
+# compile javascript
+browserify $SRC/js/main.js > $DIR/build/script.js
+
+# straight-up copy the html file
+cp $SRC/index.html $DIR/build/index.html 
+
