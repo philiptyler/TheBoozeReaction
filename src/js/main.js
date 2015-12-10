79817499f0ccaae4252a32f6b9b8aadc596982cf
Zach Rose <zach@zachrose.biz>
2015-12-10 12:19:14 -0800

    Add global event bus

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
-var foo = require('./foo');
+// We're attaching a single event bus to the global object.
+// We don't have to do this, but it aids understanding of what we're doing,
+// and makes it easier to experiment from the browser.
 
-alert(foo);
+window.bus = require('./bus');
+
