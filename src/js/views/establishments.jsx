d3f2941f3f28177a5cfa1282b9908b9352c97497
Zach Rose <zach@zachrose.biz>
2015-12-10 18:06:44 -0800

    Add working nested view for Establishments

--- src/js/views/establishments.jsx
+++ src/js/views/establishments.jsx
@@ -3,9 +3,29 @@ var ReactDOM = require('react-dom');
 var EstablishmentView = require('./establishment.jsx');
 
 module.exports = React.createClass({
+    getInitialState: function(){
+        return { data: [] };
+    },
+    componentDidMount: function(){
+        window.bus.on('ESTABLISHMENTS_READY', function(data){
+            this.setState({ data: data });
+            this.render();
+        }, this);
+    },
     render: function(){
+        var establishmentNodes = this.state.data.map(function(est){
+            return (
+                <li key={ est.id }>
+                    <h3>{ est.name }</h3>
+                    <p>{ est.description }</p>
+                </li>
+            );
+        });
         return (
-            <h1>We have many establishments!</h1>
+            <ul>
+            // <Establishment name=???/>
+            { establishmentNodes }
+            </ul>
         );
     }
 });
